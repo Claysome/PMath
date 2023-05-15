499b946b39c9e374ef9cb338b1ed5f61cd9cd6cc
clay <claysomes@outlook.com>
2023-05-15 16:46:00 +0800

    complete_vanilla

--- app/models/strats_base.py
+++ app/models/strats_base.py
@@ -4,5 +4,5 @@ from abc import ABC, abstractmethod
 class OptionPricingStrats(ABC):
     """Abstract class for option pricing strategy"""
     @abstractmethod
-    def calculate_price(self, S0, K, T, r, sigma):
+    def calculate_price(self, S0, K, T, r, sigma, otype):
         pass
