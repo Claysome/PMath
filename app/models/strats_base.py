a1a05c3fea9d7ce4f280ace8083d3dc85e030c3b
peng <haochen.peng@xrenewable.com>
2023-05-17 11:46:07 +0800

    move_dir

--- app/models/strats_base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class OptionPricingStrats(ABC):
-    """Abstract class for option pricing strategy"""
-    @abstractmethod
-    def calculate_price(self, S0, K, T, r, sigma, otype):
-        pass
